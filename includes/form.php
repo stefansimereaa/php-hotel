f047b713e98dc61b5e50bb56066463d9eb2f1725
stefansimereaa <stefanosimerea16@gmail.com>
2023-07-18 13:31:02 +0200

    added table and forms with bootstrap

--- includes/form.php
+++ includes/form.php
@@ -35,7 +35,7 @@ if (isset($_GET['vote']) && $_GET['vote'] !== '') {
 </head>
 
 <body>
-    <!DOCTYPE html>
+    <!-- <!DOCTYPE html>
     <html lang="en">
 
     <head>
@@ -68,8 +68,58 @@ if (isset($_GET['vote']) && $_GET['vote'] !== '') {
                 <?php endforeach; ?>
             </tbody>
         </table>
-    </body>
+    </body> -->
 
+    <!-- Hotels Form and Table  -->
+    <div class="container">
+
+        <h1 class="my-4">Elenco degli Hotel</h1>
+        <!-- Form per i filtri -->
+        <form method="GET" class="my-3">
+            <div class="row">
+                <div class="col-md-4 mb-3">
+                    <label for="parking">Filtra per parcheggio:</label>
+                    <select name="parking" id="parking" class="form-control">
+                        <option value="">Tutti</option>
+                        <option value="1">Con parcheggio</option>
+                        <option value="0">Senza parcheggio</option>
+                    </select>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <label for="vote">Filtra per voto (>=):</label>
+                    <input type="number" name="vote" id="vote" min="1" max="5" step="1" class="form-control">
+                </div>
+                <div class="col-md-4 mb-3">
+                    <button type="submit" class="btn btn-primary mt-4">Filtra</button>
+                </div>
+            </div>
+        </form>
+
+        <!-- Tabella degli hotel -->
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Descrizione</th>
+                    <th>Parcheggio</th>
+                    <th>Voto</th>
+                    <th>Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($filteredHotels as $hotel) : ?>
+                    <tr>
+                        <td><?= $hotel['name']; ?></td>
+                        <td><?= $hotel['description']; ?></td>
+                        <td><?= $hotel['parking'] ? 'Sì' : 'No'; ?></td>
+                        <td><?= $hotel['vote']; ?></td>
+                        <td><?= $hotel['distance_to_center']; ?> km</td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
+    </div>
 </body>
 
 </html>
