56bd7296953dc82cdad87fb08bc792b6a6a01a3d
stefansimereaa <stefanosimerea16@gmail.com>
2023-07-18 13:25:42 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// Added Data file
+include 'includes/form.php';
+?>
+
+<!-- Html -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="includes/img/hotel.png" />
+    <title>PHP Hotels</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
